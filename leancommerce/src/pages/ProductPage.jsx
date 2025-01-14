cb5a3a663be3c4c1120428ab2c39891c9983c046
Mayra Gonçalves <goncalvesmayra11@gmail.com>
2025-01-13 21:26:15 -0300

    12º: Adiciona CarrtContext

--- leancommerce/src/pages/ProductPage.jsx
+++ leancommerce/src/pages/ProductPage.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { ProductContext } from "../Context/ProductContext";
+import Product from "../components/Product";
 
-const ProductPage = () => {
+const ProductPage = ({ product }) => {
+  const { id, image, category, title, price } = product;
   return <div></div>;
 };
 
